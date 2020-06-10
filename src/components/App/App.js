45e04ceaa5e0d322b6baab6536406abc6cf93d48
fmsilva1996 <fmsilva1996@gmail.com>
2020-06-10 10:36:59 +0000

    Add basic hero component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,14 @@ import './App.css';
 
 // Components 
 import Navbar from '../Navbar/Navbar'
+import Hero from '../Hero/Hero';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero /> 
+      {/*Main Body*/}
     </div>
   );
 }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './Hero.css';
 
+// Assets
+import heroImg from '../../assets/hero.jpg'
+
+// Evergreen Components
+import { Button } from 'evergreen-ui'
+
 const Hero = () => (
-  <div className="Hero">
-    Hero Component
-  </div>
+  <section className="Hero">
+    <div className="Hero-Inner">
+      <h1>The Home of European Football</h1>
+      <h4>Get live scores, predictions and the latest news from the continent's top 5 leagues.</h4>
+      <Button>Today's Action...</Button>
+    </div>
+  </section>
 );
-
-Hero.propTypes = {};
-
-Hero.defaultProps = {};
 
 export default Hero;
