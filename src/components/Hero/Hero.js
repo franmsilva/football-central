c4ff2383305a5edeb1f6c527010b177e2e12501b
fmsilva1996 <fmsilva1996@gmail.com>
2020-06-10 09:57:37 +0000

    Add basic navbar component

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Hero.css';
+
+const Hero = () => (
+  <div className="Hero">
+    Hero Component
+  </div>
+);
+
+Hero.propTypes = {};
+
+Hero.defaultProps = {};
+
+export default Hero;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Navbar.css';
+
+// Evergreen Components
+import { Pane, Button, Text, Heading } from 'evergreen-ui'
+
+const Navbar = () => (
+  <Pane display="flex" padding={16} background="tint2" borderRadius={3}>
+    <Pane flex={1} alignItems="center" display="flex">
+      <Heading size={600}>LOGO HERE</Heading>
+    </Pane>
+    <Pane>
+      <Button marginRight={16}>Premier League</Button>
+      <Button marginRight={16}>La Liga</Button>
+      <Button marginRight={16}>Serie A</Button>
+      <Button marginRight={16}>Bundesliga</Button>
+      <Button marginRight={48}>Ligue 1</Button>
+      <Button>Login</Button>
+    </Pane>
+  </Pane>
+);
+
+export default Navbar;
