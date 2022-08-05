1d5d22c560f602f99a221156b477c0932b69f1db
Francisco Silva <fmsilva1996@gmail.com>
2022-08-05 19:22:33 +0100

    refactor: Logo component to use css modules

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import './Logo.css';
-
-// Assets 
-import icon from '../../assets/logo.svg'
+import React from "react";
+import styles from "./Logo.module.css";
 
 const Logo = () => (
-  <div className="Logo">
-    <div className="Text">
-      <span id="football">FOOTBALL</span>
-      <img alt='icon' src={icon} /> 
-      <span id="central">CENTRAL</span>
-    </div> 
-    <div className="Line"></div>
+  <div className={styles.logo}>
+    <div className={styles.text}>
+      <span className={styles.footballText}>FOOTBALL</span>
+      <img className={styles.icon} alt="icon" src="/logo.svg" />
+      <span className={styles.centralText}>CENTRAL</span>
+    </div>
+    <div className={styles.line}></div>
   </div>
 );
 
