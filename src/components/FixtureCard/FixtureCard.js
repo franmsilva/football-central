c6f96de11dc7a102337cd35d21f76ccc3fe44e7b
Francisco Silva <fmsilva1996@gmail.com>
2022-08-05 21:06:18 +0100

    refactor: FixtureCard component to use css modules

--- src/components/FixtureCard/FixtureCard.js
+++ src/components/FixtureCard/FixtureCard.js
@@ -1,83 +1,89 @@
-import React from 'react';
-import './FixtureCard.css';
-
-// Routing
-import { Link } from 'react-router-dom';
-
-// Utils
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 import moment from 'moment'
+
+import styles from './FixtureCard.css'
+import { APOSTROPHE } from '../../constants/symbols'
 
 const FixtureCard = ({ fixture }) => {
   const displayFixtureStatus = () => {
-    switch(fixture.status) {
+    switch (fixture.status) {
       case 'Not Started':
         return (
-          <React.Fragment> 
+          <>
             <p>{moment(fixture.event_date).format('DD.MM.yyyy')}</p>
             <p>{moment(fixture.event_date).format('HH:mm')}</p>
-          </React.Fragment>
+          </>
         )
-        case 'Halftime':
-          return (
-            <React.Fragment>
-              <div className='fixture__card__status halftime'>Halftime</div>
-              <div className='elapsedtime'>{fixture.elapsed}'</div>
-            </React.Fragment>
-          )
-        case 'Match Finished':
-            return (
-              <React.Fragment>
-              <div className='fixture__card__status fulltime'>Fulltime</div>
-              <div className='elapsedtime'>{fixture.elapsed}'</div>
-            </React.Fragment>
-            )
-        default: 
-          return (
-            <React.Fragment>
-              <div className='fixture__card__status live'>Live</div>
-              <div className='elapsedtime'>{fixture.elapsed}'</div>
-            </React.Fragment>
-          )
+      case 'Halftime':
+        return (
+          <>
+            <div className={[styles.fixture__card__status, styles.halftime]}>Halftime</div>
+            <div className="elapsedtime">
+              {fixture.elapsed}
+              {APOSTROPHE}
+            </div>
+          </>
+        )
+      case 'Match Finished':
+        return (
+          <>
+            <div className={[styles.fixture__card__status, styles.fulltime]}>Fulltime</div>
+            <div className="elapsedtime">
+              {fixture.elapsed}
+              {APOSTROPHE}
+            </div>
+          </>
+        )
+      default:
+        return (
+          <>
+            <div className={[styles.fixture__card__status, styles.live]}>Live</div>
+            <div className="elapsedtime">
+              {fixture.elapsed}
+              {APOSTROPHE}
+            </div>
+          </>
+        )
     }
   }
 
   return (
-    <div className='card'> 
+    <div className={styles.card}>
       <Link to={`/league/${fixture.league_id}/${fixture.league.name}`}>
-        <div className='card__header'>
-            <div className='card__header__left'> 
-              <img alt='' src={fixture.league.logo} width={25} />
-              <h3>{fixture.league.name}</h3>
-            </div>
-            <p>Matchday {fixture.round.split(' - ')[1]}</p>
+        <div className={styles.card__header}>
+          <div className={styles.card__header__left}>
+            <Image alt="" src={fixture.league.logo} width={25} />
+            <h3>{fixture.league.name}</h3>
+          </div>
+          <p>Matchday {fixture.round.split(' - ')[1]}</p>
         </div>
       </Link>
       <Link to={`/fixture/${fixture.fixture_id}`}>
-        <div className='card__body'>
-          <div className='card__body__teams'> 
-            <div className='team'>
-              <div className='team__info'> 
-                <img alt='' src={fixture.homeTeam.logo} width={30}/>
+        <div className={styles.card__body}>
+          <div className={styles.card__body__teams}>
+            <div className={styles.team}>
+              <div className={styles.team__info}>
+                <Image alt="" src={fixture.homeTeam.logo} width={30} />
                 <p>{fixture.homeTeam.team_name}</p>
               </div>
               <p>{fixture.goalsHomeTeam}</p>
             </div>
-            <div className='team'> 
-              <div className='team__info'> 
-                <img alt='' src={fixture.awayTeam.logo} width={30}/>
+            <div className={styles.team}>
+              <div className={styles.team__info}>
+                <Image alt="" src={fixture.awayTeam.logo} width={30} />
                 <p>{fixture.awayTeam.team_name}</p>
               </div>
               <p>{fixture.goalsAwayTeam}</p>
             </div>
           </div>
-          <div className='separator' />
-          <div className='card__body__time'>
-            {displayFixtureStatus()}
-          </div>
+          <div className={styles.separator} />
+          <div className={styles.card__body__time}>{displayFixtureStatus()}</div>
         </div>
       </Link>
     </div>
-  );
+  )
 }
 
-export default FixtureCard;
+export default FixtureCard
