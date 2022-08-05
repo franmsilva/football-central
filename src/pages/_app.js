c1d035ce720b7969324c12298de054cdf889351a
Francisco Silva <fmsilva1996@gmail.com>
2022-08-05 17:49:48 +0100

    feat: add _app and _document files

---
+++ src/pages/_app.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Football Central</title>
+      </Head>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <Component {...pageProps} />;
+    </>
+  );
+}
