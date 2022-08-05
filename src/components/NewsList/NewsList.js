b76bc88e157de2b2c3ce8db548a4c5f26f611fcb
Francisco Silva <fmsilva1996@gmail.com>
2022-08-05 21:32:05 +0100

    refactor: NewsList component to use css modules

--- src/components/NewsList/NewsList.js
+++ src/components/NewsList/NewsList.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import './NewsList.css';
+import React from 'react'
 
-// My Components
-import NewsCardV2 from '../NewsCardV2/NewsCardV2';
-import NewsCardLarge from '../NewsCardLarge/NewsCardLarge';
+import NewsCardV2 from '../NewsCardV2/NewsCardV2'
+import NewsCardLarge from '../NewsCardLarge/NewsCardLarge'
+import { APOSTROPHE } from '../../constants/symbols'
 
-const NewsList = ({news}) => (
-  <div className="NewsList">
-    {news.length ? <NewsCardLarge article={news[0]} /> : null} 
-    <div className='small__cards__container'>
-      {news.slice(1).map(article => {
-        return <NewsCardV2 article={article} />
+import styles from './NewsList.module.css'
+
+const NewsList = ({ news }) => (
+  <div className={styles.newsList}>
+    {news.length ? <NewsCardLarge article={news[0]} /> : null}
+    <div className={styles.small__cards__container}>
+      {news.slice(1).map((article) => {
+        return <NewsCardV2 article={article} key={article.id} />
       })}
     </div>
-    {!news.length ? <div className='backup'>You're all caught up <span role='img' aria-label=''>🤓</span></div> : null}
+    {!news.length ? (
+      <div className={styles.backup}>
+        You{APOSTROPHE}re all caught up{' '}
+        <span role="img" aria-label="">
+          🤓
+        </span>
+      </div>
+    ) : null}
   </div>
-);
+)
 
-export default NewsList;
+export default NewsList
