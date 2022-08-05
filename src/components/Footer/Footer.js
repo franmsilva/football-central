ef70d7f9bccaa47bf0b659eed1df937d3c4aa94b
Francisco Silva <fmsilva1996@gmail.com>
2022-08-05 21:09:52 +0100

    refactor: Footer component to use css modules

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './Footer.css';
+import React from 'react'
+import { COPYRIGHT } from '../../constants/symbols'
+import styles from './Footer.module.css'
 
 const Footer = () => (
-  <div className='footer'>
-    <div className='copyright'>
-      &copy; 2020 <a href='https://github.com/fmsilva1996' rel="noopener noreferrer" target="_blank">fmsilva</a> 
+  <div className={styles.footer}>
+    <div>
+      {COPYRIGHT} {new Date().getFullYear()}{' '}
+      <a href="https://github.com/fmsilva1996" rel="noopener noreferrer" target="_blank">
+        fmsilva
+      </a>
     </div>
   </div>
-);
+)
 
-export default Footer;
+export default Footer
