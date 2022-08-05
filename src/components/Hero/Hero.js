a7976cb470413f83da695f13f9dc032995b62190
Francisco Silva <fmsilva1996@gmail.com>
2022-08-05 21:15:18 +0100

    refactor: Hero component to use css modules

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import './Hero.css';
+import React from 'react'
+
+import { APOSTROPHE } from '../../constants/symbols'
+
+import styles from './Hero.module.css'
 
 const Hero = () => (
-  <div className="hero">
+  <div className={styles.hero}>
     <h1>
-      <span>HOME OF EUROPEAN FOOTBALL</span><br />
-      Get live scores, predictions and the latest news from the continent's top domestic leagues
+      <span>HOME OF EUROPEAN FOOTBALL</span>
+      <br />
+      Get live scores, predictions and the latest news from the continent{APOSTROPHE}s top domestic
+      leagues
     </h1>
 
-    <div className="mouse">
+    <div className={styles.mouse}>
       <span></span>
     </div>
   </div>
-);
+)
 
-export default Hero;
+export default Hero
