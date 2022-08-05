abed58d23267a2f889fa8220d55fb0134fff6690
Francisco Silva <fmsilva1996@gmail.com>
2022-08-05 19:22:49 +0100

    refactor: Navbar component to use css modules

--- src/components/NavbarV2/NavbarV2.js
+++ src/components/NavbarV2/NavbarV2.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import './NavbarV2.css';
+import React from "react";
+import Link from "next/link";
+import styles from "./NavbarV2.module.css";
 
 // My Components
-import Logo from '../Logo/Logo';
-import { Link } from 'react-router-dom';
+import Logo from "../Logo/Logo";
 
 const NavbarV2 = () => {
   return (
-    <header className="main_h sticky">
-      <div className="row">
-        <Link to='/'>
+    <header className={[styles.main_h, styles.sticky]}>
+      <div className={styles.row}>
+        <Link href="/">
           <Logo />
         </Link>
         <div className="container borderXwidth">
-          <Link to='/league/524/Premier-League'>Premier League</Link>
-          <Link to='/league/775/La-Liga'>La Liga</Link>
-          <Link to='/league/754/Bundesliga'>Bundesliga</Link>
-          <Link to='/league/891/Serie-A'>Serie A</Link>
-          <Link to='/league/525/Ligue-1'>Ligue 1</Link>
-          <Link to='/league/766/Primeira-Liga'>Primeira Liga</Link>
+          <Link href="/league/524/Premier-League">Premier League</Link>
+          <Link href="/league/775/La-Liga">La Liga</Link>
+          <Link href="/league/754/Bundesliga">Bundesliga</Link>
+          <Link href="/league/891/Serie-A">Serie A</Link>
+          <Link href="/league/525/Ligue-1">Ligue 1</Link>
+          <Link href="/league/766/Primeira-Liga">Primeira Liga</Link>
         </div>
-      </div> 
+      </div>
     </header>
   );
-}
+};
 
 export default NavbarV2;
