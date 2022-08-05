2a68b59db2b70bb5bcb3ba2e2176f1a591a9be62
Francisco Silva <fmsilva1996@gmail.com>
2022-08-05 21:21:18 +0100

    refactor: Spinner component to use css modules

--- src/components/NewsCardLarge/NewsCardLarge.js
+++ src/components/NewsCardLarge/NewsCardLarge.js
@@ -1,29 +1,34 @@
-import React from 'react';
-import './NewsCardLarge.css';
-
-// Utils 
+import React from 'react'
+import Image from 'next/image'
 import moment from 'moment'
 
-// My Components 
-import MainSpinner from '../MainSpinner/MainSpinner'
+import Spinner from '../Spinner/Spinner'
 
-const NewsCardLarge = ({article}) => (
-  <React.Fragment> 
-    {article.source
-      ? <div className="card__large">
-          <img className="card__large__img" src={article.urlToImage} alt=''/>
-          <a href={article.url} rel="noopener noreferrer" target="_blank" >
-            <div className="card__large__info">
-              <span className="card__large__source">{article.source.name.split(' - ')[0]}</span>
-              <h3 className="card__large__title">{article.title.split(' - ')[0]}</h3>
-              <h4 className="card__large__description">{article.description}</h4>
-              <span className="card__large__published">published: <span className='card__large__date'>{moment(article.publishedAt).format('DD.MM.YYYY')}</span></span>
-            </div>
-          </a>
-        </div>
-      : <MainSpinner />
-    }
-  </React.Fragment>
-);
+import './NewsCardLarge.css'
 
-export default NewsCardLarge;
+const NewsCardLarge = ({ article }) => (
+  <>
+    {article.source ? (
+      <div className="card__large">
+        <Image className="card__large__img" src={article.urlToImage} alt="" />
+        <a href={article.url} rel="noopener noreferrer" target="_blank">
+          <div className="card__large__info">
+            <span className="card__large__source">{article.source.name.split(' - ')[0]}</span>
+            <h3 className="card__large__title">{article.title.split(' - ')[0]}</h3>
+            <h4 className="card__large__description">{article.description}</h4>
+            <span className="card__large__published">
+              published:{' '}
+              <span className="card__large__date">
+                {moment(article.publishedAt).format('DD.MM.YYYY')}
+              </span>
+            </span>
+          </div>
+        </a>
+      </div>
+    ) : (
+      <Spinner />
+    )}
+  </>
+)
+
+export default NewsCardLarge
