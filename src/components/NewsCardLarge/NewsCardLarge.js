c87d831bc81b4f52df496d5d6282851fb2e444c8
Francisco Silva <fmsilva1996@gmail.com>
2022-08-05 21:25:59 +0100

    refactor: NewsCardLarge component to use css modules

--- src/components/NewsCardLarge/NewsCardLarge.js
+++ src/components/NewsCardLarge/NewsCardLarge.js
@@ -4,21 +4,23 @@ import moment from 'moment'
 
 import Spinner from '../Spinner/Spinner'
 
-import './NewsCardLarge.css'
+import styles from './NewsCardLarge.module.css'
 
 const NewsCardLarge = ({ article }) => (
   <>
     {article.source ? (
-      <div className="card__large">
-        <Image className="card__large__img" src={article.urlToImage} alt="" />
+      <div className={styles.card__large}>
+        <Image className={styles.card__large__img} src={article.urlToImage} alt="" />
         <a href={article.url} rel="noopener noreferrer" target="_blank">
-          <div className="card__large__info">
-            <span className="card__large__source">{article.source.name.split(' - ')[0]}</span>
-            <h3 className="card__large__title">{article.title.split(' - ')[0]}</h3>
-            <h4 className="card__large__description">{article.description}</h4>
-            <span className="card__large__published">
+          <div className={styles.card__large__info}>
+            <span className={styles.card__large__source}>
+              {article.source.name.split(' - ')[0]}
+            </span>
+            <h3 className={styles.card__large__title}>{article.title.split(' - ')[0]}</h3>
+            <h4 className={styles.card__large__description}>{article.description}</h4>
+            <span className={styles.card__large__published}>
               published:{' '}
-              <span className="card__large__date">
+              <span className={styles.card__large__date}>
                 {moment(article.publishedAt).format('DD.MM.YYYY')}
               </span>
             </span>
