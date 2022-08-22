e425c0d0dfc5cd40c4e29af26d98d18e85b66abf
Francisco Silva <fmsilva1996@gmail.com>
2022-08-22 15:59:16 +0000

    feat: setup Storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,68 @@
+import * as NextImage from 'next/image';
+import React from 'react';
+
+import '../src/styles/globals.css';
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+const customViewports = {
+  mobileSmall: {
+    name: 'Mobile Small',
+    styles: {
+      width: '320px',
+      height: '848px',
+    },
+  },
+  mobileMedium: {
+    name: 'Mobile Medium',
+    styles: {
+      width: '375px',
+      height: '848px',
+    },
+  },
+  mobileLarge: {
+    name: 'Tablet',
+    styles: {
+      width: '425px',
+      height: '848px',
+    },
+  },
+  tablet: {
+    name: 'Tabled',
+    styles: {
+      width: '768px',
+      height: '848px',
+    },
+  },
+  laptop: {
+    name: 'Laptop',
+    styles: {
+      width: '1024',
+      height: '848px',
+    },
+  },
+};
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  viewport: {
+    defaultViewport: customViewports.mobileMedium,
+    viewports: customViewports,
+  },
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'Black',
+    values: [{ name: 'Black', value: '#000000' }],
+  },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
