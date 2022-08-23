73ba0b0d65d046b2133e59546c05175c9dcae075
Francisco Silva <fmsilva1996@gmail.com>
2022-08-23 20:31:12 +0000

    fix: storybook ts alias paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(ts|tsx)'],
@@ -24,22 +24,12 @@ module.exports = {
   core: {
     builder: '@storybook/builder-webpack5',
   },
-  webpackFinal: (config) => {
-    /**
-     * Add support for alias-imports
-     * @see https://github.com/storybookjs/storybook/issues/11989#issuecomment-715524391
-     */
-    config.resolve.alias = {
-      ...config.resolve?.alias,
-      '@': [path.resolve(__dirname, '../src/'), path.resolve(__dirname, '../')],
-    };
-
-    /**
-     * Fixes font import with /
-     * @see https://github.com/storybookjs/storybook/issues/12844#issuecomment-867544160
-     */
-    config.resolve.roots = [path.resolve(__dirname, '../public'), 'node_modules'];
-
+  /**
+   * Fix TypeScript alias paths
+   * @see https://github.com/storybookjs/storybook/issues/6316#issuecomment-659726964
+   */
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [new TsconfigPathsPlugin()];
     return config;
   },
 };
